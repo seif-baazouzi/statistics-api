66a24fcfad542e30ee724bb3edfbd5d224ba21fd
seif <baazouziseif9@gmail.com>
2022-07-12 09:00:05 +0100

    added pagination to get logs route

--- src/routes/logs/get.js
+++ src/routes/logs/get.js
@@ -8,14 +8,30 @@ const { checkCollectionOwner } = require("../../utils/check-collection-owner")
 
 logs.get("/:collectionID", tokens.auth(), checkCollectionOwner(), (req, res) => {
   trycatch(req, res, async () => {
-    const { rows } = await db.query(
-      "SELECT logID, label, value, logDate FROM logs WHERE collectionID = $1",
+    const page = req.query.page ?? 1;
+    
+    const limit = 20;
+    const offset = limit * (page -1);
+
+    // get pages
+
+    const { rows: pagesRow } = await db.query(
+      "SELECT count(*) as count FROM logs WHERE collectionID = $1",
       [ req.params.collectionID ]
+    )
+
+    const pages = Math.floor(pagesRow[0].count / limit);
+    
+    // get logs
+
+    const { rows } = await db.query(
+      "SELECT logID, label, value, logDate FROM logs WHERE collectionID = $1 LIMIT $2 OFFSET $3",
+      [ req.params.collectionID, limit, offset ]
     )
 
     const logs = rows.map(log => ({ logID: log.logid, label: log.label, value: log.value, logDate: log.logdate }))
     
-    res.json({ logs })
+    res.json({ logs, pages })
   })
 })
 
